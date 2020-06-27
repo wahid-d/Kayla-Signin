564cef44d58b07fca821e1d6dfb867571415c0c7
wahid-d <wakhid2802@gmail.com>
2020-06-27 19:57:49 +0900

    Initial

---
+++ SignInPage/Views/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SignInPage.Views;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace SignInPage.Views
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new SignUp());
+        }
+
+        void LoginClicked(System.Object sender, System.EventArgs e)
+        {
+            if (Preferences.ContainsKey("username") && Preferences.Get("username", "") == username.Text)
+            {
+                if(Preferences.Get("password", "") == password.Text)
+                {
+                    DisplayAlert("Login Successful", $"{username.Text} is signed in!", "OK");
+                }
+
+            }
+            else
+            {
+                DisplayAlert("Login Failed", $"{username.Text} NOT exist!", "OK");
+            }
+
+        }
+    }
+}
---
+++ SignInPage/Views/SignUp.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace SignInPage.Views
+{
+    public partial class SignUp : ContentPage
+    {
+        public SignUp()
+        {
+            InitializeComponent();
+        }
+
+        async void SignupClicked(System.Object sender, System.EventArgs e)
+        {
+            if(string.IsNullOrEmpty(username.Text) ||
+                string.IsNullOrEmpty(email.Text) ||
+                string.IsNullOrEmpty(password.Text) ||
+                string.IsNullOrEmpty(confirm.Text))
+            {
+                DisplayAlert("Error", "Fill in all fields", "OK");
+                return;
+            }
+
+            if(password.Text != confirm.Text)
+            {
+                DisplayAlert("Error", "passwords dont match", "OK");
+                return;
+            }
+
+            Preferences.Set("username", username.Text);
+            Preferences.Set("password", password.Text);
+            Preferences.Set("email", email.Text);
+
+            DisplayAlert("Congrats", "Successfully signed UP!", "OK");
+
+            await Navigation.PopAsync();
+        }
+    }
+}
